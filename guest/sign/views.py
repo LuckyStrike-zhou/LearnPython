8df75c8e58e8dae53ac04117de72b2c5081ce458
zhoutong <>
2019-01-08 14:46:50 +0800

    split

---
+++ guest/sign/views.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    # return HttpResponse("Hello,Django!!")
+    return render(request, "index.html")
+
+#登录动作
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        if username == 'admin' and password == 'admin123':
+            return HttpResponse('login success')
+        else:
+            return render(request, 'index.html', {'error': 'username or password error!'})
